07b144124e2594d5d220f1f164ceda0d3cfbeee8
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-07-02 17:20:47 +0100

    create basic multiplying calculator

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int firstInput = Convert.ToInt32 ( Console.ReadLine () );
+			int secondInput = Convert.ToInt32 ( Console.ReadLine () );
+			Console.WriteLine (firstInput * secondInput);
+		}
+	}
+}
